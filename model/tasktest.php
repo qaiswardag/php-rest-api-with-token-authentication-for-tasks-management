a737deb10e3e23fe53c73800cb8ac79d7c9c4a18
Qais Wardag <47112831+qaiswardag@users.noreply.github.com>
2022-04-02 13:13:13 +0200

    Fix spelling

--- model/tasktest.php
+++ model/tasktest.php
@@ -3,7 +3,7 @@
 require_once 'Task.php';
 
 
-// 1: Test: successfull
+// 1: Test: successfully
 try {
     $task = new Task(1, "Title here", "Description here", "01/01/2019 12:00", "N");
     header('Content-type: application/json;charset=UTF-8');
@@ -14,7 +14,7 @@ try {
 }
 
 
-//// 2: Test: not successfull
+//// 2: Test: not successfully
 //try {
 //    $task = new Task(1, "Title here", "Description here", "01/01/2019 12:00", "O");
 //    header('Content-type: application/json;charset=UTF-8');
