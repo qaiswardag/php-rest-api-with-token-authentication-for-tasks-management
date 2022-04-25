5e9148fdcf1434f1d8aa15f030df42880d4c708f
Qais Wardag <47112831+qaiswardag@users.noreply.github.com>
2022-04-25 15:22:31 +0200

    .

---
+++ controller/sessions.php
@@ -0,0 +1,1557 @@
+<?php
+require_once 'db.php';
+require_once '../model/Response.php';
+
+// try
+try {
+    $writeDB = DB::connectWriteDB();
+
+} catch (PDOException $e) {
+    error_log("Connection Error: " . $e, 0);
+    $response = new Response();
+    // http code 500: server error. can not connect
+    $response->setHttpStatusCode(500);
+    $response->setSuccess(false);
+    $response->addMessage('Database connection error');
+    $response->send();
+    exit;
+}
+
+// handle options request method for CORS
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    header('Access-Control-Allow-Methods: POST, OPTIONS');
+    header('Access-Control-Allow-Headers: Content-Type');
+    header('Access-Control-Max-age: 86400');
+    $response = new Response();
+    $response->setHttpStatusCode(200);
+    $response->setSuccess(true);
+    $response->addMessage('Preflight OPTIONS check');
+    $response->send();
+    exit;
+}
+
+// to do:
+// sessions = POST - create a session / login
+// sessions/3 = DELETE - log out a user
+// sessions/3 = PATCH - refresh session
+// sessions/test = error: endpoint not found
+
+if (array_key_exists("sessionid", $_GET)) {
+}
+
+// if GET request is empty
+// handle creating new session, e.g. log in
+if (empty($_GET)) {
+    // handle creating new session, e.g. logging in
+    // check to make sure the request is POST only - else exit with error response
+    if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+        $response = new Response();
+        $response->setHttpStatusCode(405);
+        $response->setSuccess(false);
+        $response->addMessage("Request method not allowed");
+        $response->send();
+        exit;
+    }
+
+    // insert a delay on every login etempt for 1 second for security popurse. using function sleep
+    // delay login by 1 second to slow down any potential brute force attacks
+    sleep(1);
+
+// check if content type is set to JSON
+    if (!isset($_SERVER['CONTENT_TYPE']) || (isset($_SERVER['CONTENT_TYPE']) && $_SERVER['CONTENT_TYPE'] !== 'application/json')) {
+        // set up response for unsuccessful request
+        $response = new Response();
+        $response->setHttpStatusCode(400);
+        $response->setSuccess(false);
+        $response->addMessage("Content Type header not set to JSON");
+        $response->send();
+        exit;
+    }
+
+// Content-Type: application/json is the content header. the content header is just information
+// make sure data provided is valid json
+// get POST request body as the POSTed data will be JSON format
+    $rawPostData = file_get_contents('php://input');
+
+// decode the data provided and check if the decode method has not returned false
+// if the data is true it will be stored in the $jsonData variable
+    if (!$jsonData = json_decode($rawPostData)) {
+        $response = new Response();
+        // http code 400: client error
+        $response->setHttpStatusCode(400);
+        $response->setSuccess(false);
+        $response->addMessage('Request body is not valid JSON');
+        $response->send();
+        exit;
+    }
+
+// if data i valid JSON
+    if (!isset($jsonData->username) || !isset($jsonData->password)) {
+        $response = new Response();
+        // http code 400: client error
+        $response->setHttpStatusCode(400);
+        $response->setSuccess(false);
+        (!isset($jsonData->username) ? $response->addMessage('Username not supplied') : false);
+        (!isset($jsonData->password) ? $response->addMessage('Password not supplied') : false);
+        $response->send();
+        exit;
+    }
+
+// username and password validation
+    if (strlen($jsonData->username) < 1 || strlen($jsonData->username) > 255 || strlen($jsonData->password) < 1 || strlen($jsonData->password) > 255) {
+        $response = new Response();
+        // http code 400: client error
+        $response->setHttpStatusCode(400);
+        $response->setSuccess(false);
+        (strlen($jsonData->username) < 1 ? $response->addMessage('Username can not be blank') : false);
+        (strlen($jsonData->username) > 255 ? $response->addMessage('Username can must be less than 255 characters') : false);
+
+        (strlen($jsonData->password) < 1 ? $response->addMessage('Password can not be blank') : false);
+        (strlen($jsonData->password) > 255 ? $response->addMessage('Password can must be less than 255 characters') : false);
+        $response->send();
+        exit;
+    }
+
+
+    try {
+        // create a query that will return a row from database
+        $username = $jsonData->username;
+        $password = $jsonData->password;
+
+        $query = $writeDB->prepare('SELECT id, fullname, username, password, useractive, loginattempts from tblusers where username = :username');        // we should max get 1 row back as username is unique
+        $query->bindParam(':username', $username, PDO::PARAM_STR);
+        $query->execute();
+
+        $rowCOunt = $query->rowCount();
+
+        // if row cound is 0, means no user exists in the database with that username
+        if ($rowCOunt === 0) {
+            $response = new Response();
+            // http code 401: unauthorized error
+            $response->setHttpStatusCode(401);
+            $response->setSuccess(false);
+            $response->addMessage('Username or password is incorrect');
+            $response->send();
+            exit;
+        }
+
+        // if user exists
+        $row = $query->fetch(PDO::FETCH_ASSOC);
+
+        // save returned details into variables
+        $returned_id = $row['id'];
+        $returned_fullname = $row['fullname'];
+        $returned_username = $row['username'];
+        $returned_password = $row['password'];
+        $returned_useractive = $row['useractive'];
+        $returned_loginattempts = $row['loginattempts'];
+
+        if ($returned_useractive !== 'Y') {
+            $response = new Response();
+            // http code 401: unauthorized error
+            $response->setHttpStatusCode(401);
+            $response->setSuccess(false);
+            $response->addMessage('User account not active');
+            $response->send();
+            exit;
+        }
+
+        // count number of login attempts
+//        if ($returned_loginattempts >= 3) {
+        if ($returned_loginattempts >= 30) {
+            $response = new Response();
+            // http code 401: unauthorized error
+            $response->setHttpStatusCode(401);
+            $response->setSuccess(false);
+            $response->addMessage('User account is currently locked');
+            $response->send();
+            exit;
+        }
+
+
+        // validate password
+        // the password in database is hashed so we need to use the method password_verified
+        // we can not convert the hashed password back to plain text password
+        // check if password is the same using the hash
+        if (!password_verify($password, $returned_password)) {
+            // create the query to increment attempts figure
+            $query = $writeDB->prepare('update tblusers set loginattempts = loginattempts+1 where id = :id');
+            // bind the user id
+            $query->bindParam(':id', $returned_id, PDO::PARAM_INT);
+            // run the query
+            $query->execute();
+
+            // send response
+            $response = new Response();
+            $response->setHttpStatusCode(401);
+            $response->setSuccess(false);
+            $response->addMessage("Username or password is incorrect");
+            $response->send();
+            exit;
+        }
+
+
+        // create the access and refresh token
+        // the method makes sure that the value have not been used before
+        // the value is bytes
+        // we need to convert to bin2hex method is used
+        // generate readable characters base64_encode method is used
+
+        // to make sure no one can copy and access token from client device we suffix it with the time method
+        // we add time on the top of the value which is returned
+        $accesstoken = base64_encode(bin2hex(openssl_random_pseudo_bytes(24)) . time());
+
+        // refresh token
+        $refreshtoken = base64_encode(bin2hex(openssl_random_pseudo_bytes(24)) . time());
+
+        // access token expiry - 20 min
+        $access_token_expiry_seconds = 1200;
+        // refresh token expiry - 14 days
+        $refresh_token_expiry_seconds = 1209600;
+
+
+    } catch (PDOException $e) {
+        $response = new Response();
+        // http code 500: server error
+        $response->setHttpStatusCode(500);
+        $response->setSuccess(false);
+        $response->addMessage('There was an issue logging in');
+        $response->send();
+        exit;
+    }
+
+
+    //
+    try {
+        $writeDB->beginTransaction();
+
+        $query = $writeDB->prepare('update tblusers set loginattempts = 0 where id = :id');
+        $query->bindParam(':id', $returned_id, PDO::PARAM_INT);
+        $query->execute();
+
+        $query = $writeDB->prepare('insert into tblsessions (userid, accesstoken, accesstokenexpiry, refreshtoken, refreshtokenexpiry) values (:userid, :accesstoken, date_add(NOW(), INTERVAL  :accesstokenexpiryseconds SECOND), :refreshtoken, date_add(NOW(), INTERVAL  :refreshtokenexpiryseconds SECOND))');
+        $query->bindParam(':userid', $returned_id, PDO::PARAM_INT);
+        $query->bindParam(':accesstoken', $accesstoken, PDO::PARAM_STR);
+        $query->bindParam(':accesstokenexpiryseconds', $access_token_expiry_seconds, PDO::PARAM_INT);
+        $query->bindParam(':refreshtoken', $refreshtoken, PDO::PARAM_STR);
+        $query->bindParam(':refreshtokenexpiryseconds', $refresh_token_expiry_seconds, PDO::PARAM_INT);
+        $query->execute();
+
+
+        // get unique session id
+        $lastSessionID = $writeDB->lastInsertId();
+
+        // cause we are using beginTransaction method we need to commit or save the data
+        $writeDB->commit();
+
+        $returnData = array();
+        $returnData['session_id'] = intval($lastSessionID);
+        $returnData['access_token'] = $accesstoken;
+        $returnData['access_token_expires_in'] = $access_token_expiry_seconds;
+        $returnData['refresh_token'] = $refreshtoken;
+        $returnData['refresh_token_expires_in'] = $refresh_token_expiry_seconds;
+
+        $response = new Response();
+        // http code 201: created something - session
+        $response->setHttpStatusCode(201);
+        $response->setSuccess(true);
+        $response->setData($returnData);
+        $response->send();
+        exit;
+
+
+        // catch
+    } catch (PDOException $e) {
+
+        $writeDB->rollBack();
+
+
+        $response = new Response();
+        // http code 500: database error
+        $response->setHttpStatusCode(500);
+        $response->setSuccess(false);
+        $response->addMessage('There was an issue logging in. Please try again');
+        $response->send();
+        exit;
+    }
+
+// check if we can find a user with the passed in username
+} else {
+    $response = new Response();
+    // http code 404: does not exists
+    $response->setHttpStatusCode(404);
+    $response->setSuccess(false);
+    $response->addMessage('Endpoint not found');
+    $response->send();
+    exit;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ controller/users.php
@@ -0,0 +1,172 @@
+<?php
+
+require_once('db.php');
+require_once('../model/response.php');
+
+// note: never cache user http requests/responses
+// (our response model defaults to no cache unless specifically set)
+
+// attempt to set up connections to db connections
+try {
+
+    $writeDB = DB::connectWriteDB();
+
+} catch (PDOException $ex) {
+    // log connection error for troubleshooting and return a json error response
+    error_log("Connection Error: " . $ex, 0);
+    $response = new Response();
+    $response->setHttpStatusCode(500);
+    $response->setSuccess(false);
+    $response->addMessage("Database connection error");
+    $response->send();
+    exit;
+}
+
+// handle options request method for CORS
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    header('Access-Control-Allow-Methods: POST, OPTIONS');
+    header('Access-Control-Allow-Headers: Content-Type');
+    header('Access-Control-Max-age: 86400');
+    $response = new Response();
+    $response->setHttpStatusCode(200);
+    $response->setSuccess(true);
+    $response->addMessage('Preflight OPTIONS check');
+    $response->send();
+    exit;
+}
+
+// handle creating new user
+// check to make sure the request is POST only - else exit with error response
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    $response = new Response();
+    $response->setHttpStatusCode(405);
+    $response->setSuccess(false);
+    $response->addMessage("Request method not allowed");
+    $response->send();
+    exit;
+}
+
+// check request's content type header is JSON
+if ($_SERVER['CONTENT_TYPE'] !== 'application/json') {
+    // set up response for unsuccessful request
+    $response = new Response();
+    $response->setHttpStatusCode(400);
+    $response->setSuccess(false);
+    $response->addMessage("Content Type header not set to JSON");
+    $response->send();
+    exit;
+}
+
+// get POST request body as the POSTed data will be JSON format
+$rawPostData = file_get_contents('php://input');
+
+if (!$jsonData = json_decode($rawPostData)) {
+    // set up response for unsuccessful request
+    $response = new Response();
+    $response->setHttpStatusCode(400);
+    $response->setSuccess(false);
+    $response->addMessage("Request body is not valid JSON");
+    $response->send();
+    exit;
+}
+
+// check if post request contains full name, username and password in body as they are mandatory
+if (!isset($jsonData->fullname) || !isset($jsonData->username) || !isset($jsonData->password)) {
+    $response = new Response();
+    $response->setHttpStatusCode(400);
+    $response->setSuccess(false);
+    // add message to message array where necessary
+    (!isset($jsonData->fullname) ? $response->addMessage("Full name not supplied") : false);
+    (!isset($jsonData->username) ? $response->addMessage("Username not supplied") : false);
+    (!isset($jsonData->password) ? $response->addMessage("Password not supplied") : false);
+    $response->send();
+    exit;
+}
+
+// check to make sure that full name username and password are not empty and less than 255 long
+if (strlen($jsonData->fullname) < 1 || strlen($jsonData->fullname) > 255 || strlen($jsonData->username) < 1 || strlen($jsonData->username) > 255 || strlen($jsonData->password) < 1 || strlen($jsonData->password) > 100) {
+    $response = new Response();
+    $response->setHttpStatusCode(400);
+    $response->setSuccess(false);
+    (strlen($jsonData->fullname) < 1 ? $response->addMessage("Full name cannot be blank") : false);
+    (strlen($jsonData->fullname) > 255 ? $response->addMessage("Full name cannot be greater than 255 characters") : false);
+    (strlen($jsonData->username) < 1 ? $response->addMessage("Username cannot be blank") : false);
+    (strlen($jsonData->username) > 255 ? $response->addMessage("Username cannot be greater than 255 characters") : false);
+    (strlen($jsonData->password) < 1 ? $response->addMessage("Password cannot be blank") : false);
+    (strlen($jsonData->password) > 100 ? $response->addMessage("Password cannot be greater than 100 characters") : false);
+    $response->send();
+    exit;
+}
+
+// trim any leading and trailing blank spaces from full name and username only - password may contain a leading or trailing space
+$fullname = trim($jsonData->fullname);
+$username = trim($jsonData->username);
+$password = $jsonData->password;
+
+// attempt to query the database to check if username already exists
+try {
+    // create db query
+    $query = $writeDB->prepare('SELECT id from tblusers where username = :username');
+    $query->bindParam(':username', $username, PDO::PARAM_STR);
+    $query->execute();
+
+    // get row count
+    $rowCount = $query->rowCount();
+
+    if ($rowCount !== 0) {
+        // set up response for username already exists
+        $response = new Response();
+        $response->setHttpStatusCode(409);
+        $response->setSuccess(false);
+        $response->addMessage("Username already exists");
+        $response->send();
+        exit;
+    }
+
+    // hash the password to store in the DB as plain text password stored in DB is bad practice
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+    // create db query to create user
+    $query = $writeDB->prepare('INSERT into tblusers (fullname, username, password) values (:fullname, :username, :password)');
+    $query->bindParam(':fullname', $fullname, PDO::PARAM_STR);
+    $query->bindParam(':username', $username, PDO::PARAM_STR);
+    $query->bindParam(':password', $hashed_password, PDO::PARAM_STR);
+    $query->execute();
+
+    // get row count
+    $rowCount = $query->rowCount();
+
+    if ($rowCount === 0) {
+        // set up response for error
+        $response = new Response();
+        $response->setHttpStatusCode(500);
+        $response->setSuccess(false);
+        $response->addMessage("There was an error creating the user account - please try again");
+        $response->send();
+        exit;
+    }
+
+    // get last user id so we can return the user id in the json
+    $lastUserID = $writeDB->lastInsertId();
+
+    // build response data array which contains basic user details
+    $returnData = array();
+    $returnData['user_id'] = $lastUserID;
+    $returnData['fullname'] = $fullname;
+    $returnData['username'] = $username;
+
+    $response = new Response();
+    $response->setHttpStatusCode(201);
+    $response->setSuccess(true);
+    $response->addMessage("User created");
+    $response->setData($returnData);
+    $response->send();
+    exit;
+} catch (PDOException $ex) {
+    $response = new Response();
+    $response->setHttpStatusCode(500);
+    $response->setSuccess(false);
+    $response->addMessage("There was an issue creating a user account - please try again");
+    $response->send();
+    exit;
+}
